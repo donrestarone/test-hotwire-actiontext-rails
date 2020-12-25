bfde063bef5890634c13a41e30af727cbe5df785
donrestarone <shashikejayatunge@gmail.com>
2020-12-25 08:06:59 -0500

    implement turbo for tweet crud actions

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,7 @@
 class Tweet < ApplicationRecord
   has_rich_text :content
+
+  after_create_commit {broadcast_prepend_to "tweets"}
+  after_update_commit {broadcast_replace_to "tweets"}
+  after_destroy_commit {broadcast_remove_to "tweets"}
 end
